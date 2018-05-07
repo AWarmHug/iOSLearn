5639140b92dae439864d170bf24585056744f0e5
RenBin <865741452@qq.com>
2018-05-07 17:17:00 +0800

    init

---
+++ Swift/Basis/Basis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Basis
+//
+//  Created by Warm-mac on 2018/5/7.
+//  Copyright © 2018 me.warm. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
