694a378219cf839e67dfdbe606a0fbec0fdca218
RenBin <865741452@qq.com>
2018-05-08 18:24:26 +0800

    String相关内容

--- Swift/Basis/Basis/ViewController.swift
+++ Swift/Basis/Basis/ViewController.swift
@@ -29,18 +29,92 @@ class ViewController: UIViewController {
         var yuanzu: (code: Int, msg: String)
 
         yuanzu = (code: 200, msg: "请求成功")
-
-        //需要在看看
+        //5、需要在看看
         //可选项，nil 一个对象可能为空，就用nil来表示，在定义对象的时候可以通过，!和?来表示，如：
 
         //表示money不可以为nil
         var money: String! = "20";
 
-        if let myMoney = Int(money) {
+        //6、可选项绑定 在判断时使用，如果转换成立，则赋值，let和var都支持,
+        //判断中支持多个可选项同时绑定，使用'，'分割，其中只要一个发生错误，就为false
+        if var myMoney = Int(money) {
             debugPrint("moneyIsNil=\(myMoney),money是数字")
         } else {
             debugPrint("money不是数字")
         }
+
+        //7、区间运算符 闭区间运算符 a...b ，
+        //半开区间 运算符 a..<b，
+        //单侧区间 a.../...a 从一边的某个位置一直到另外一边尽可能远
+        //区间一般用于循环，或者配合数组等数据结构一起使用
+
+        let names = ["张三", "李四", "王二", "陆八"]
+
+        //闭区间，包含a和b，并且a<=b
+        debugPrint("区间运算符")
+        for item in 1...2 {
+            debugPrint(names[item])
+        }
+        for item in names[1...2] {
+            debugPrint(item)
+        }
+        //半开区间，包含a，不包含b，a<=b
+        debugPrint("半开区间")
+        for item in 0..<names.count {
+            debugPrint(names[item])
+        }
+        for item in names[0..<names.count] {
+            debugPrint(item)
+        }
+//        这样就会出错
+//        for item in 4..<1 {
+//            debugPrint(item)
+//        }
+        //单侧区间 一般配合数组一起使用
+        debugPrint("单侧区间")
+        for item in names[1...] {
+            debugPrint(item)
+        }
+        for item in names[...2] {
+            debugPrint(item)
+        }
+        //8、String "str"，
+        //多行表示 """
+        //        str
+        //        str
+        //        """
+        //在字符串中使用引号，需要使用\转义
+        var str: String;
+        str = "str";
+        print(str)
+        str = """
+        str
+        str
+        str
+        """;
+        print(str)
+        str = "张三说：\"你好\""
+        print(str)
+
+        str = "abcdefghij"
+        print(str.startIndex)
+        print(str.endIndex)
+        //从e到结尾 包含e
+        print(str.substring(from: str.index(of: "e")!))
+        //从开头到e，不包含e
+        print(str.substring(to: str.index(of: "e")!))
+
+
+        print(str.substring(with: str.index(str.startIndex, offsetBy: 2)..<str.index(str.endIndex, offsetBy: -4)))
+
+
+        str.filter { (character: Character) -> Bool in
+                    (character != "b")
+                }
+                .compactMap { (character: Character) in
+                    print(character)
+                }
+
     }
 
 
