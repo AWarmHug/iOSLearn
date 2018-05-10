0503e82d5d1c01ad334e9ee6fef2d098e497910c
RenBin <865741452@qq.com>
2018-05-10 09:04:06 +0800

    新建Basis2

---
+++ Swift/Basis2/Basis2/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Basis2
+//
+//  Created by Warm-mac on 2018/5/10.
+//  Copyright © 2018 me.warm. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //循环语句 for in
+        for name in ["张三", "王二", "李四", "陆八"] {
+            print(name)
+        }
+
+
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
